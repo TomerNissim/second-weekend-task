381620b39745638a0c88180bcea80c469c7fb96b
TomerNissim <tomernis@gmail.com>
2021-01-23 11:42:17 +0200

    task complete

--- weekend-task.js
+++ weekend-task.js
@@ -8,45 +8,69 @@ let objArray = [{topic: "HTML" ,startedAt: new Date("2021-01-21:13:00"),finished
 {topic: "Cyber4s" ,startedAt: new Date("2021-01-16:01:00"),finishedAt: new Date("2021-01-16:23:00"),tasksGiven: 200, tasksFinished: 199},
 {topic: "conditions" ,startedAt: new Date("2021-01-15:10:00"),finishedAt: new Date("2021-01-15:14:30"),tasksGiven: 25, tasksFinished: 19},
 {topic: "Objects" ,startedAt: new Date("2021-01-15:16:00"),finishedAt: new Date("2021-01-15:18:30"),tasksGiven: 10, tasksFinished: 8}];
+
+for (let object of objArray){
+    object.tasksFinishedPercent = Math.floor(object.tasksFinished / object.tasksGiven *100);
+    object.totalTime = timeCalculate(object.startedAt, object.finishedAt);
+}
+document.write(`<table class ="table"> 
+    <tr class = "tableRowHeader">
+        <th class ="tableCollomHeader">started At</th> 
+        <th class ="tableCollomHeader">finished At</th> 
+        <th class ="tableCollomHeader">Total Time Spent</th> 
+        <th class ="tableCollomHeader">Tasks Given</th> 
+        <th class ="tableCollomHeader">Tasks Finished</th> 
+        <th class ="tableCollomHeader">Tasks Finished %</th> 
+        <th class ="tableCollomHeader">Topic</th>
+    </tr>
+ </table>`);
+
+for(let object of objArray){
+    creatTableRows(object);
+}
+function creatTableRows(obj){
+   let totalTimeColor = changeBackgroundColorTotalTime(obj.totalTime);
+   let PercentColor = changeBackgroundColorTaskFinishedPrecent(obj.tasksFinishedPercent)
+    document.write(`<table class ="table"> 
+        <tr class = "tableRow">
+            <td class ="tableCollom">${convertDate(obj.startedAt)}</td>
+            <td class ="tableCollom">${convertDate(obj.finishedAt)}</td> 
+            <td class ="tableCollom" style=${totalTimeColor}>${obj.totalTime}</td> 
+            <td class ="tableCollom">${obj.tasksGiven}</td> 
+            <td class ="tableCollom">${obj.tasksFinished}</td> 
+            <td class ="tableCollom" style=${PercentColor}>${obj.tasksFinishedPercent}% </td> 
+            <td class ="tableCollom">${obj.topic}</td>   
+        </tr>
+    </table>`);
+}
+
 function timeCalculate(startDate, finishedDate){
     let startTime = startDate.getHours() + (startDate.getMinutes()/60);
     let finishedTime = finishedDate.getHours() + (finishedDate.getMinutes()/60);
     return finishedTime - startTime;
 }
-for (let object of objArray){
-    object.tasksFinishedPrecent = Math.floor(object.tasksFinished / object.tasksGiven *100);
-    object.totalTime = timeCalculate(object.startedAt, object.finishedAt);
-}
-
- document.write(`<table class ="table"> 
-        <tr class = "tableRowHeader">
-            <th class ="tableCollomHeader">started At</th> 
-            <th class ="tableCollomHeader">finished At</th> 
-            <th class ="tableCollomHeader">Total Time Spent</th> 
-            <th class ="tableCollomHeader">Tasks Given</th> 
-            <th class ="tableCollomHeader">Tasks Finished</th> 
-            <th class ="tableCollomHeader">Tasks Finished %</th> 
-            <th class ="tableCollomHeader">Topic</th>
-        </tr>
-    </table>`);
-
-function creatTableRows(obj){
-    document.write(`<table class ="table"> 
-        <tr class = "tableRow">
-            <td class ="tableCollom" >${convertDate(obj.startedAt)}</td>
-            <td class ="tableCollom">${convertDate(obj.finishedAt)}</td> 
-            <td class ="tableCollom">${obj.totalTime}</td> 
-            <td class ="tableCollom">${obj.tasksGiven}</td> 
-            <td class ="tableCollom">${obj.tasksFinished}</td> 
-            <td class ="tableCollom">${obj.tasksFinishedPrecent}</td> 
-            <td class ="tableCollom">${obj.topic}</td>   
-        </tr>
-    </table>`);
-}
 function convertDate(date){
     return date.toString().substr(16,5);
 }
-
-for(let object of objArray){
-    creatTableRows(object);
+function changeBackgroundColorTotalTime(time){
+    if(time > 10){
+        return "background-color:#ec0c0c";
+    }
+    else  if(time > 5){
+        return "background-color:#ec9e0c";
+    }
+    else {
+        return "background-color:#0cec2d";
+    }
+}
+function changeBackgroundColorTaskFinishedPrecent(time){
+    if(time >= 80){
+        return "background-color:#77056d";
+    }
+    else  if(time > 60){
+        return "background-color:#ec0ce7";
+    }
+    else {
+        return "background-color:#e98fe7";
+    }
 }
